7cf4fa8032a9ee0f8df66819bca4c46fe7a23abb
Gopal Menon <gopal.mnn@gmail.com>
2017-04-02 16:33:56 -0600

    Finished 1A

--- A5_1A.m
+++ A5_1A.m
@@ -1,4 +1,10 @@
-cd /Users/gopalmenon/Documents/workspace/CS-6140-A5-Regression/data/
+cd data
 load('A.dat')
-[U,S,V] = svd(A)Uk = U(:,1:k)Sk = S(1:k,1:k)Vk = V(:,1:k)Ak = Uk*Sk*Vk’
-norm(A-Ak,2)
+for k=(1:10)
+  [U,S,V] = svd(A);
+  Uk = U(:,1:k);
+  Sk = S(1:k,1:k);
+  Vk = V(:,1:k);
+  Ak = Uk*Sk*Vk';
+  disp(norm(A-Ak,2));
+endfor
