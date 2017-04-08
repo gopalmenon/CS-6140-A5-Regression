b9ce1a7834face7cfac4038a8bed69dda4a0802a
Gopal Menon <gopal.mnn@gmail.com>
2017-04-08 12:43:58 -0600

    Changes in script. Plus removed data files

--- A5_1A.m
+++ A5_1A.m
@@ -8,3 +8,11 @@ for k=(1:10)
   Ak = Uk*Sk*Vk';
   disp(norm(A-Ak,2));
 endfor
+# 1C - Find the first two eigen vectors
+twoDVectors = V(1:end,1:2)
+# Projection of all points on the eigen vectors
+PointsToPlot = A*twoDVectors
+scatter(PointsToPlot(1:end,1), PointsToPlot(1:end, 2)); hold on
+xlabel('Coordinate 1');
+ylabel('Coordinate 2');
+title('Plot that minimizes sum of residuals squared')
