699513ac349b02dfcec19847c0365eaefe7b714c
Gopal Menon <gopal.mnn@gmail.com>
2017-04-08 23:51:27 -0600

    Finished 2A

--- FD.m
+++ FD.m
@@ -29,45 +29,60 @@ function [reduced_matrix] = frequent_directions(input_matrix, desired_rank)
   [input_rows, input_dimensions] = size(input_matrix);
 
   # Initialize return value with zeros
-  reduced_matrix = zeros(2*desired_rank, input_dimensions);
+  reduced_matrix = zeros(desired_rank, input_dimensions);
 
   # Repeat for each row in input matrix
   for row_counter = 1:rows(input_matrix)
     
     # Look for a row in the reduced matrix with all zeros
     [found_row, zero_row] = get_zero_row(reduced_matrix);
-    
-    if (found_row)
 
-      # Replace the zero row with a row from input matrix
-      reduced_matrix(zero_row, :) = input_matrix(row_counter, :);
+    # Replace the zero row with a row from input matrix
+    reduced_matrix(zero_row, :) = input_matrix(row_counter, :);
     
-    else 
+    # Check if reduced matrix has any empty rows
+    [found_row, zero_row] = get_zero_row(reduced_matrix);
     
+    if (!found_row)
+    
+      # Singular value decomposition
       [U,S,V] = svd(reduced_matrix);
-      size(S)
-      if (S(2*desired_rank,2*desired_rank) != 0)
+
+      if (S(min(input_dimensions,desired_rank), min(input_dimensions,desired_rank)) != 0)
       
-        last_eigen_value =  S(2*desired_rank,2*desired_rank) ^ 2;
+        last_eigen_value =  S(min(input_dimensions,desired_rank), min(input_dimensions,desired_rank)) ^ 2;
         new_singular_matrix = zeros(size(S));
       
         # Change each singular value
-        for singular_value_counter = 1:input_dimensions
-        
+        for singular_value_counter = 1:min(input_dimensions,desired_rank)
           new_singular_matrix(singular_value_counter, singular_value_counter) = sqrt(S(singular_value_counter, singular_value_counter)^2 - last_eigen_value);
-      
-      
         endfor
+
+        # Reduce the matrix
         reduced_matrix =  new_singular_matrix * V.';
-      endif
+
+        endif
+        
     endif
   
   endfor
   
 end
 
+# Return k approximation of input matrix
+function [best_k_approximation] = get_best_k_approximation(input_matrix, k)
+
+  [U,S,V] = svd(input_matrix);
+  Uk = U(:,1:k);
+  Sk = S(1:k,1:k);
+  Vk = V(:,1:k);
+  best_k_approximation = Uk*Sk*Vk';
+
+end
+
 #cd data;
 load('A.dat');
 #A=[1,2,3;0,0,0;5,6,7;9,7,4];
-B= frequent_directions(A,500);
+B= frequent_directions(A,10);
 norm(A'*A - B'*B, 2)
+(norm(A - get_best_k_approximation(A, 2),"fro")^2)/10
